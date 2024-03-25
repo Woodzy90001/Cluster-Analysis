69595ce0632d1d4f034b6ceaf429a60dc144b137
Woodzy90001 <129260695+Woodzy90001@users.noreply.github.com>
2024-03-25 14:34:38 +0800

    Update run_cluster.sh

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash --login
-#SBATCH --account=p87
+#SBATCH --account=group-code
 #SBATCH --partition=work
 #SBATCH --time=4:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --job-name=PIMD-test
+#SBATCH --job-name=PIMD-runs
 
-lammpsexe=/software/projects/p87/cwood1/LAMMPS/mylammps/PIMD-build/lmp
+lammpsexe=lammps-directory
 
 srun -N 1 -n 1 -c 1 -m block:block:block ${lammpsexe} -in PIMD_$1.in > PIMD_$1.out
